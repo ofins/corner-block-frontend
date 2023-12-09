4112d83e644ff72f42e57fdfe83b897b24d309d5
ofins <jkyeewang@gmail.com>
2023-12-09 10:39:58 +0800

    [fix] update home logs

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="bg-bg-asPrimary w-full flex justify-center items-center <md:flex-col my-100px">
-    <div>
+    <div class="py-5 flex-col-center">
       <img
         src="/icons/icons8-cube-100.png"
         class="animate-bounce-alt animate-count-infinite animate-duration-3s"
@@ -9,37 +9,53 @@
         <p>
           Built by
           <a
-            class="font-italic decoration-none c-inherit"
+            class="font-italic c-inherit"
             target="_blank"
-            href="https://twitter.com/ofinsNFT"
+            href="https://id.roninchain.com/name/ofins.ron"
             >ofins.ron</a
           >
+        </p>
+        <p>
+          Come say hi! 👋 ->
+          <a target="_blank" class="c-inherit" href="https://twitter.com/ofinsNFT">my twitter</a>
         </p>
       </div>
     </div>
     <div class="c-text-asPrimary max-w-500px md:ml-100px px-16px flex flex-col justify-start gap-2">
       <div class="b-1px b-solid b-white p-16px rd-regular b-op-30">
-        <p>Important:</p>
-        <p>- CornerBlock is powered by CoinGecko's free API, update frequency is every minute or so.</p>
-        <p>- Currently search term only supports coin id (bitcoin, ethereum) and <u>NOT</u> coin symbols (BTC, ETH). If you do not know the coin id for your token, you can find it on:<br/> CoinGecko -> click on a token -> api id.</p>
-      </div>
-      <div class="b-1px b-solid b-white p-16px rd-regular b-op-30">
-        <p class="mt-16px">Version 2023.12.4</p>
-        <ul class="text-start">
-          <li>Allow multi-editing of existing block through modal</li>
-          <li>increase blocks number from 5 to 11</li>
-          <li>added glowing border when block is selected</li>
-          <li>prices updates correctly</li>
-          <li>fix minor bugs</li>
+        <p class="text-20px"><u>Important</u>:</p>
+        <ul>
+          <li>
+            CornerBlock is powered by CoinGecko's free API, update frequency is every minute or so.
+          </li>
+          <li>
+            Currently search term only supports coin id (bitcoin, ethereum) and <u>NOT</u> coin
+            symbols (BTC, ETH). If you do not know the coin id for your token, you can find it
+            on:<br />
+            CoinGecko -> click on a token -> api id.
+          </li>
         </ul>
       </div>
-      <div class="b-1px b-solid b-white p-16px rd-regular b-op-30">
-        <p class="mt-16px c-primary">Version 2023.12.6</p>
+      <!-- <div class="b-1px b-solid b-white p-16px rd-regular b-op-30">
+        <p class="mt-16px">Version 2023.12.6</p>
         <ul class="text-start">
           <li>redesign theme color and style</li>
           <li>search queries now works with hyphens</li>
           <li>added percentage change stats to top right of block detail</li>
           <li>block detail style changed to fit dark mode theme</li>
+          <li>fix minor bugs</li>
+        </ul>
+      </div> -->
+      <div class="b-1px b-solid b-white p-16px rd-regular b-op-30">
+        <p class="text-20px mt-16px c-primary">Version 2023.12.9</p>
+        <ul class="text-start">
+          <li>temporarily remove sidebar transition (causing bugs)</li>
+          <li>add clear ALL tokens button in sidebar</li>
+          <li>added new button styles</li>
+          <li>
+            NEW feature: ability to add total holding amount for each coin and display it on blocks.
+            Can hide value in general setting.
+          </li>
           <li>fix minor bugs</li>
         </ul>
       </div>
@@ -50,9 +66,7 @@
 <script setup lang="ts"></script>
 
 <style scoped>
-
 li {
-  list-style:circle;
+  list-style: circle;
 }
-
 </style>
