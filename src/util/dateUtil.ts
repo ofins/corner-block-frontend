07ed571aa3424b84b0e0c8eb6fa53f7741ff421b
ofins <jkyeewang@gmail.com>
2023-12-25 13:04:31 +0800

    [feat] dateUtil

---
+++ src/util/dateUtil.ts
@@ -0,0 +1,14 @@
+// export const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+
+export const updateFormattedDateTime = () => {
+  const currentDate = new Date()
+
+  const year = currentDate.getFullYear()
+  const month = currentDate.getMonth() + 1
+  const day = currentDate.getDate()
+  const hours = currentDate.getHours()
+  const minutes = currentDate.getMinutes()
+  const seconds = currentDate.getSeconds()
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`
+}
