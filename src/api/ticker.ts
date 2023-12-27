dde4cafb64207f6ea98f02048925790d4b92c7dc
ofins <jkyeewang@gmail.com>
2023-12-27 22:31:25 +0800

    [fix] fix demo api endpoint

--- src/api/ticker.ts
+++ src/api/ticker.ts
@@ -1,4 +1,5 @@
 import { fetchData } from './index'
+import { DEMO_10K_API_KEY } from '@/settings/apiBaseEndPoint'
 
 const GET_TICKERS_PRICES = '/simple/price?ids='
 const SET_CURRENCY = '&vs_currencies='
@@ -7,9 +8,17 @@ const GET_MULTI_TICKERS_DETAIL = '/markets?vs_currency=usd&ids='
 const NO_SPARKLINE_LOCALE_EN = '&sparkline=false&locale=en'
 
 export const getTickerPrice = (tickers: string, currency: string = 'usd') =>
-  fetchData(GET_TICKERS_PRICES + tickers + SET_CURRENCY + currency)
+  fetchData(GET_TICKERS_PRICES + tickers + SET_CURRENCY + currency + '&' + DEMO_10K_API_KEY)
 
-export const getTickerDetail = (ticker: string) => fetchData(GET_TICKER_DETAIL + ticker)
+export const getTickerDetail = (ticker: string) =>
+  fetchData(GET_TICKER_DETAIL + ticker + '?' + DEMO_10K_API_KEY)
 
 export const getMultiTickersDetail = (tickers: string, currency: string = 'usd') =>
-  fetchData(GET_TICKER_DETAIL + GET_MULTI_TICKERS_DETAIL + tickers + NO_SPARKLINE_LOCALE_EN)
+  fetchData(
+    GET_TICKER_DETAIL +
+      GET_MULTI_TICKERS_DETAIL +
+      tickers +
+      NO_SPARKLINE_LOCALE_EN +
+      '&' +
+      DEMO_10K_API_KEY
+  )
