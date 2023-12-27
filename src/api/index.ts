e447d5ba9ba20a7d5ee57690b801fb7c97dbc5ad
ofins <jkyeewang@gmail.com>
2023-12-27 17:46:40 +0800

    [feat] demo 10k api key

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 import apiBaseEndPoint from '@/settings/apiBaseEndPoint'
 
+const DEMO_10K_API_KEY = '&x_cg_demo_api_key=CG-Ss9UawjweBDXeWyuJXUcSHu3'
+
 export const fetchData = async (params: string) => {
-  return axios.get(apiBaseEndPoint + params)
+  return axios.get(apiBaseEndPoint + params + DEMO_10K_API_KEY)
 }
