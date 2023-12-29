0fbebcb3c536f6bcae3b4e48f4f1c8f14da282b9
ofins <jkyeewang@gmail.com>
2023-12-30 00:14:51 +0800

    [refactor] ticker - URLSearchParams, ENDPOINTS, buildUrl

--- src/api/ticker.ts
+++ src/api/ticker.ts
@@ -1,24 +1,32 @@
 import { fetchData } from './index'
 import { DEMO_10K_API_KEY } from '@/settings/apiBaseEndPoint'
 
-const GET_TICKERS_PRICES = '/simple/price?ids='
-const SET_CURRENCY = '&vs_currencies='
-const GET_TICKER_DETAIL = '/coins/'
-const GET_MULTI_TICKERS_DETAIL = '/markets?vs_currency=usd&ids='
-const NO_SPARKLINE_LOCALE_EN = '&sparkline=false&locale=en'
+const ENDPOINTS = {
+  SIMPLE: '/simple',
+  PRICE: '/price',
+  COINS: '/coins',
+  MARKETS: '/markets'
+}
+
+const buildUrl = (endpoint: string, params = {}) => {
+  const paramString = new URLSearchParams(params)
+  return `${endpoint}?${paramString.toString()}&${DEMO_10K_API_KEY}`
+}
 
 export const getTickerPrice = (tickers: string, currency: string = 'usd') =>
-  fetchData(GET_TICKERS_PRICES + tickers + SET_CURRENCY + currency + '&' + DEMO_10K_API_KEY)
+  fetchData(
+    buildUrl(`${ENDPOINTS.SIMPLE}${ENDPOINTS.PRICE}`, { ids: tickers, vs_currencies: currency })
+  )
 
 export const getTickerDetail = (ticker: string) =>
-  fetchData(GET_TICKER_DETAIL + ticker + '?' + DEMO_10K_API_KEY)
+  fetchData(buildUrl(`${ENDPOINTS.COINS}/${ticker}`))
 
 export const getMultiTickersDetail = (tickers: string, currency: string = 'usd') =>
   fetchData(
-    GET_TICKER_DETAIL +
-      GET_MULTI_TICKERS_DETAIL +
-      tickers +
-      NO_SPARKLINE_LOCALE_EN +
-      '&' +
-      DEMO_10K_API_KEY
+    buildUrl(`${ENDPOINTS.COINS}${ENDPOINTS.MARKETS}`, {
+      vs_currency: currency,
+      ids: tickers,
+      sparkline: false,
+      locale: 'en'
+    })
   )
